1b6a76213b6e1f97b6320369498b7df98b6af85c
Kartik <kartikmandhan14@gmail.com>
2021-03-13 16:10:22 +0530

    Implemented OOP

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 // gridContainer.add
-function createNode() {
-  var node = document.createElement("div");
-  node.setAttribute("class", "box");
-  return node;
-}
+class Grid {
+  createNode() {
+    let node = document.createElement("div");
+    node.setAttribute("class", "box");
+    return node;
+  }
 
-function createEmptyBoard() {
-  gridContainer = document.querySelector(".grid-container");
-  //   console.log(gridContainer.offsetWidth, gridContainer.offsetHeight);
-  //   divided by 30 since each sqaure is 30px*30px
-  var rows = Math.round(gridContainer.offsetHeight / 30) - 5;
-  var cols = Math.round(gridContainer.offsetWidth / 30) - 7;
-  console.log(rows, cols);
-  var rowsize = rows;
-  var colsize = cols;
-  gridContainer.style.setProperty("--rows", rows);
-  gridContainer.style.setProperty("--cols", cols);
-  gridContainer.innerHTML = "";
-  for (var row = 0; row < rowsize; row++) {
-    for (var col = 0; col < colsize; col++) {
-      let temp = createNode(row, col);
-      gridContainer.appendChild(temp);
+  createEmptyBoard() {
+    const gridContainer = document.querySelector(".grid-container");
+    //   console.log(gridContainer.offsetWidth, gridContainer.offsetHeight);
+    //   divided by 30 since each sqaure is 30px*30px
+    let rows = Math.round(gridContainer.offsetHeight / 30) - 5;
+    let cols = Math.round(gridContainer.offsetWidth / 30) - 7;
+    console.log(rows, cols);
+    let rowsize = rows;
+    let colsize = cols;
+    // setting css variables
+    gridContainer.style.setProperty("--rows", rows);
+    gridContainer.style.setProperty("--cols", cols);
+    gridContainer.innerHTML = "";
+    for (let row = 0; row < rowsize; row++) {
+      for (let col = 0; col < colsize; col++) {
+        let temp = this.createNode(row, col);
+        gridContainer.appendChild(temp);
+      }
     }
   }
 }
-createEmptyBoard();
+const mainGrid = new Grid();
+mainGrid.createEmptyBoard();
