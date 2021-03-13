83a422f6d09877586680b2018a5fe09598e419b6
Kartik <kartikmandhan14@gmail.com>
2021-03-13 17:12:02 +0530

    Feat: Added walls mouse events

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ class Grid {
   createNode() {
     let node = document.createElement("div");
     node.setAttribute("class", "box");
+    node.addEventListener("click", () => {
+      node.classList.toggle("isWall");
+    });
     return node;
   }
-
+  ismousePressed = false;
   createEmptyBoard() {
     const gridContainer = document.querySelector(".grid-container");
     //   console.log(gridContainer.offsetWidth, gridContainer.offsetHeight);
     //   divided by 30 since each sqaure is 30px*30px
-    let rows = Math.round(gridContainer.offsetHeight / 30) - 5;
-    let cols = Math.round(gridContainer.offsetWidth / 30) - 7;
+    gridContainer.addEventListener("mousedown", () => {
+      this.ismousePressed = true;
+    });
+    gridContainer.addEventListener("mouseup", () => {
+      this.ismousePressed = false;
+    });
+    gridContainer.addEventListener("mouseover", (e) => {
+      // console.log(e.target);
+      if (this.ismousePressed) {
+        if (e.target.classList.contains("box")) {
+          e.target.classList.toggle("isWall");
+        }
+      }
+    });
+    let rows = Math.round(gridContainer.offsetHeight / 30) - 3;
+    let cols = Math.round(gridContainer.offsetWidth / 30) - 5;
     console.log(rows, cols);
     let rowsize = rows;
     let colsize = cols;
