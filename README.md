7f91ab8da725fe6aba5a79da0dfe9fd521b3e821
Kartik <kartikmandhan14@gmail.com>
2021-03-14 11:11:02 +0530

    Created README and start and EndIcon

---
+++ README.md
@@ -0,0 +1,9 @@
+# AlgoVisualizer
+1. Fork this Repo
+2. Clone your forked repo
+3. Make changes in the Code
+4. run command : git add .
+5. run command : git commit -m "meaningful commit message".
+6. run command : git pull upstream master
+7. run command : git push origin master.
+8. generate a pull request.
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // gridContainer.add
 class Grid {
-  createNode() {
+  createNode(row, col) {
     let node = document.createElement("div");
     node.setAttribute("class", "box");
+    node.setAttribute("row", row);
+    node.setAttribute("col", col);
     node.addEventListener("click", () => {
       node.classList.toggle("isWall");
     });
@@ -29,16 +31,24 @@ class Grid {
     });
     let rows = Math.round(gridContainer.offsetHeight / 30) - 3;
     let cols = Math.round(gridContainer.offsetWidth / 30) - 5;
-    console.log(rows, cols);
-    let rowsize = rows;
-    let colsize = cols;
+    // console.log(rows, cols);
     // setting css variables
     gridContainer.style.setProperty("--rows", rows);
     gridContainer.style.setProperty("--cols", cols);
     gridContainer.innerHTML = "";
-    for (let row = 0; row < rowsize; row++) {
-      for (let col = 0; col < colsize; col++) {
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
         let temp = this.createNode(row, col);
+        if (row === 10 && col == 10) {
+          let startIcon = document.createElement("i");
+          startIcon.setAttribute("class", "bi bi-nut-fill Icon");
+          temp.appendChild(startIcon);
+        }
+        if (row === 30 && col == 30) {
+          let endIcon = document.createElement("i");
+          endIcon.setAttribute("class", "bi bi-geo-alt-fill Icon");
+          temp.appendChild(endIcon);
+        }
         gridContainer.appendChild(temp);
       }
     }
