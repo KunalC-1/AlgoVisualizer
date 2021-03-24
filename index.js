03b49a9ddc4383771f842e8cf204c06b05a40ce9
Kartik <kartikmandhan14@gmail.com>
2021-03-24 10:39:30 +0530

    start and endBlock cannot be made wall anymore

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ class Grid {
     node.setAttribute("class", "box");
     node.setAttribute("row", row);
     node.setAttribute("col", col);
+    node.setAttribute("wall", 0);
+
     node.addEventListener("click", () => {
-      node.classList.toggle("isWall");
+      if (
+        node.classList.contains("box") &&
+        node.id !== "startBox" &&
+        node.id !== "endBox"
+      )
+        node.classList.toggle("isWall");
+      if (node.getAttribute("wall") == "0") {
+        node.setAttribute("wall", 1);
+      } else {
+        node.setAttribute("wall", 0);
+      }
     });
     return node;
   }
@@ -22,31 +34,43 @@ class Grid {
       this.ismousePressed = false;
     });
     gridContainer.addEventListener("mouseover", (e) => {
-      // console.log(e.target);
       if (this.ismousePressed) {
-        if (e.target.classList.contains("box")) {
+        // console.log(e.target);
+
+        if (
+          e.target.classList.contains("box") &&
+          e.target.id !== "startBox" &&
+          e.target.id !== "endBox"
+        ) {
           e.target.classList.toggle("isWall");
+          if (node.getAttribute("wall") == "0") {
+            node.setAttribute("wall", 1);
+          } else {
+            node.setAttribute("wall", 0);
+          }
         }
       }
     });
-    let rows = Math.round(gridContainer.offsetHeight / 30) - 3;
-    let cols = Math.round(gridContainer.offsetWidth / 30) - 5;
+    this.rows = Math.round(gridContainer.offsetHeight / 30) - 3;
+    this.cols = Math.round(gridContainer.offsetWidth / 30) - 5;
     // console.log(rows, cols);
     // setting css variables
-    gridContainer.style.setProperty("--rows", rows);
-    gridContainer.style.setProperty("--cols", cols);
+    gridContainer.style.setProperty("--rows", this.rows);
+    gridContainer.style.setProperty("--cols", this.cols);
     gridContainer.innerHTML = "";
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
         let temp = this.createNode(row, col);
         if (row === 10 && col == 10) {
           let startIcon = document.createElement("i");
           startIcon.setAttribute("class", "bi bi-nut-fill Icon");
+          temp.setAttribute("id", "startBox");
           temp.appendChild(startIcon);
         }
-        if (row === 30 && col == 30) {
+        if (row === 10 && col == 30) {
           let endIcon = document.createElement("i");
           endIcon.setAttribute("class", "bi bi-geo-alt-fill Icon");
+          temp.setAttribute("id", "endBox");
           temp.appendChild(endIcon);
         }
         gridContainer.appendChild(temp);
@@ -56,3 +80,4 @@ class Grid {
 }
 const mainGrid = new Grid();
 mainGrid.createEmptyBoard();
+console.log(mainGrid.rows, mainGrid.cols);
