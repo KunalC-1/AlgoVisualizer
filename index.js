b4a053f0fa639528b6d5276c9198641350600ab1
Kartik <kartikmandhan14@gmail.com>
2021-03-28 13:17:54 +0530

    basic Theme implemented

--- index.js
+++ index.js
@@ -175,6 +175,28 @@ startButton.addEventListener("click", () => {
       break;
   }
 });
+const toggleSwitch = document.querySelector(".theme-switch input");
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme);
 
 export const rows = mainGrid.rows;
 export const cols = mainGrid.cols;
